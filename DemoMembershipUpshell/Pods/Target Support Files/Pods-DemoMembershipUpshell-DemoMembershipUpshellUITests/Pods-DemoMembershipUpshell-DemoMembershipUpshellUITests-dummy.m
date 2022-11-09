a534187f9954612fa88f4cd281e62866a8eb629a
MahvishS <msyed@thumbtack.com>
2022-11-09 10:19:34 -0500

    pod setup

---
+++ DemoMembershipUpshell/Pods/Target Support Files/Pods-DemoMembershipUpshell-DemoMembershipUpshellUITests/Pods-DemoMembershipUpshell-DemoMembershipUpshellUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoMembershipUpshell_DemoMembershipUpshellUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoMembershipUpshell_DemoMembershipUpshellUITests
+@end
